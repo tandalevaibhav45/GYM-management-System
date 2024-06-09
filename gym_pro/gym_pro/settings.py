80532e93af5d1b4b3d5020ba990ea8c9910daf86
vaibhav tandale <tandalevaibhav45@gmail.com>
2024-06-09 17:34:00 +0530

    add comment in setting

--- gym_pro/gym_pro/settings.py
+++ gym_pro/gym_pro/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+"""my project is related gym management"""
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
