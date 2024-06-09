5950d85be6cd5120488fe8ab89568238ee1a4e86
vaibhav tandale <tandalevaibhav45@gmail.com>
2024-06-09 17:11:48 +0530

    change name

--- gym_pro/gym_pro/settings.py
+++ gym_pro/gym_pro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--qa6=my9g0mop@*6u+cy%b4rfj84g&609fhrlhb@84mx==zg)^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 #"192.168.1.4"
 
 
